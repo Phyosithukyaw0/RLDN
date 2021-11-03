f7bbdf549c0795ab6c6ae4619eb330793940e794
Phyosithukyaw0 <87802365+Phyosithukyaw0@users.noreply.github.com>
2021-11-02 23:42:07 -0600

    Create RLDN.sh

---
+++ RLDN.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -p "Enter your domain website:: " domain
+red='\033[0;21m'
+blue='\033[0;34m'
+printf  "\t\t\t$blue--------$domain-----------\n\n"
+
+for  ip in $(cat subdomain.txt);do 
+ host $ip.$domain |grep "has address" |cut -d " " -f4,1 |sort -u
+done 
+   echo "Crd by Phyosithukyaw"
